eeab1654de64b489056edd582778c6ef6ce87222
Armando Maya <stronquens@gmail.com>
2017-02-07 17:51:14 +0100

    Cambiados estilos de las tarjetas

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -4,18 +4,3 @@ function adjust_textarea(h) {
     h.style.height = "20px";
     h.style.height = (h.scrollHeight)+"px";
 }
-
-
-function scrollTo(element, to, duration) {
-    if (duration <= 0) return;
-    var difference = to - element.scrollTop;
-    var perTick = difference / duration * 10;
-
-    setTimeout(function() {
-        element.scrollTop = element.scrollTop + perTick;
-        if (element.scrollTop === to) return;
-        scrollTo(element, to, duration - 10);
-    }, 10);
-}
-var elmnt = document.getElementsByClassName("preguntas");
-scrollTo(document.body, elmnt.offsetTop, 600);
